4ef2e34d4dc693fb17d4a57eb6e6cab53c281bd5
bardium <114866826+bardium@users.noreply.github.com>
2023-07-23 08:55:11 -0400

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
-	version = '1.01',
+	version = '1.02',
 	updated = '7/23/2023',
-	message = 'daruma game freeze delay + message if no rocket found'
+	message = 'disappear from hide and seek'
 }
