6955110ad86bf2320aa0853b2763fe9ae6d75065
bardium <114866826+bardium@users.noreply.github.com>
2023-07-24 05:41:54 -0400

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
-	version = '1.05',
-	updated = '7/23/2023',
-	message = 'tp to other team in dodgeball'
+	version = '1.06',
+	updated = '7/24/2023',
+	message = 'removed disappear from dodgeball as you need kills to win'
 }
