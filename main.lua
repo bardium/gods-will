47abb267724cffeb93d7d11091f4f6875ceb0758
Bardium <114866826+bardium@users.noreply.github.com>
2023-08-19 21:59:56 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 --[[
 CREDITS:
 UI Library: Inori & wally
-Script: goosebetter
+Script: bardium
 ]]
 
 repeat
@@ -547,7 +547,7 @@ Groups.Troll:AddToggle('DeleteChairAura', { Text = 'Delete Chair Aura' })
 
 Groups.Credits = Tabs.UISettings:AddRightGroupbox('Credits')
 
-addRichText(Groups.Credits:AddLabel('<font color="#0bff7e">Goose Better</font> - script'))
+addRichText(Groups.Credits:AddLabel('<font color="#0bff7e">bardium</font> - script'))
 addRichText(Groups.Credits:AddLabel('<font color="#3da5ff">wally & Inori</font> - ui library'))
 
 Groups.UISettings = Tabs.UISettings:AddRightGroupbox('UI Settings')
